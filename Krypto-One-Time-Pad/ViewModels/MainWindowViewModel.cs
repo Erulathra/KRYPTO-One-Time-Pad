af1a041fc1a02e5d4367c6ee11a9617110d1cdb1
Szymon Świędrych <szymek_sw@outlook.com>
2022-03-08 18:06:51 +0100

    Add properties to MainWindowViewModel.cs

--- Krypto-One-Time-Pad/ViewModels/MainWindowViewModel.cs
+++ Krypto-One-Time-Pad/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Subjects;
 using System.Text;
+using Avalonia.Logging;
+using JetBrains.Annotations;
+using ReactiveUI;
 
 namespace Krypto_One_Time_Pad.ViewModels
 {
-	public class MainWindowViewModel : ViewModelBase
+	public class MainWindowViewModel : ReactiveObject
 	{
-		public string Greeting => "Welcome to Avalonia!";
+		private string plainText = "Tutaj możesz wpisać tekst jawny w formie UTF-8";
+		private int plainTextLenght = 0;
+		private string cipherText = "Tutaj możesz wpisać szyfrogram w formie UTF-8";
+		private int cipherTextLenght = 0;
+		private string key  = "Tutaj możesz wpisać klucz w formie UTF-8";
+		private int keyLenght = 0;
+
+		
+
+		public string PlainText
+		{
+			get => plainText;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref plainText, value);
+				PlainTextLenght = plainText.Length;
+			}
+		}
+
+		public int PlainTextLenght
+		{
+			get => plainTextLenght;
+			set => this.RaiseAndSetIfChanged(ref plainTextLenght, value);
+		}
+		public string CipherText
+		{
+			get => cipherText;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref cipherText, value);
+				CipherTextLenght = CipherText.Length;
+			}
+		}
+
+		public int CipherTextLenght
+		{
+			get => cipherTextLenght;
+			set => this.RaiseAndSetIfChanged(ref cipherTextLenght, value);
+		}
+		
+		public string Key
+		{
+			get => key;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref key, value);
+				KeyLenght = key.Length;
+			}
+		}
+
+		public int KeyLenght
+		{
+			get => keyLenght;
+			set => this.RaiseAndSetIfChanged(ref keyLenght, value);
+		}
 	}
 }
--- Krypto-One-Time-Pad/Views/MainWindow.axaml.cs
+++ Krypto-One-Time-Pad/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Krypto_One_Time_Pad.ViewModels;
 
 namespace Krypto_One_Time_Pad.Views
 {
@@ -7,6 +8,7 @@ namespace Krypto_One_Time_Pad.Views
 		public MainWindow()
 		{
 			InitializeComponent();
+			this.DataContext = new MainWindowViewModel();
 		}
 	}
 }
